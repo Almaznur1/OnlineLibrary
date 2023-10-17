c20d79bec77c9115a943e4ae597cb8d10640d05a
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-10-17 22:22:04 +0300

    download books from all pages

---
+++ parse_tululu_category.py
@@ -0,0 +1,99 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse, unquote
+from pathlib import Path
+import time
+import sys
+import json
+from parse_tululu import download_image, download_txt, check_for_redirect
+
+
+def fetch_fantasy_books_url_with_id():
+    base_url = 'https://tululu.org/'
+    book_pages_url_with_id = []
+    for page in range(1, 5):
+        url = f'https://tululu.org/l55/{page}'
+        response = requests.get(url)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.text, 'lxml')
+        books = soup.find('div', id='content').find_all('table')
+
+        for book in books:
+            book_id = book.find('a')['href']
+            book_page_url = urljoin(base_url, book_id)
+            book_pages_url_with_id.append((book_page_url, book_id[2:-1]))
+
+    return book_pages_url_with_id
+
+
+def parse_book_page(book_page_url, response):
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    title_tag = soup.find('div', id='content').find('h1')
+    title_end_index = title_tag.text.index('::')
+    title = title_tag.text[:title_end_index].strip()
+
+    author = title_tag.text[title_end_index + 2:].strip()
+
+    image = soup.find('div', class_='bookimage').find('img')['src']
+    image_url = urljoin(book_page_url, image)
+
+    temporary_path = urlparse(unquote(image_url)).path
+    img_scr = f'images/{temporary_path[temporary_path.rfind("/") + 1:]}'
+
+    book_path = f'{title}.txt'
+
+    comment_tags = soup.find_all('div', class_='texts')
+    comments = [comment_tag.span.text for comment_tag in comment_tags]
+
+    genres_tag = soup.find('span', class_='d_book').find_all('a')
+    genres = [genre_tag.text for genre_tag in genres_tag]
+    book = {
+        'title': title,
+        'author': author,
+        'image_url': image_url,
+        'img_scr': img_scr,
+        'book_path': book_path,
+        'comments': comments,
+        'genres': genres,
+    }
+    return book
+
+
+def main():
+    Path('./books').mkdir(parents=True, exist_ok=True)
+    Path('./images').mkdir(parents=True, exist_ok=True)
+
+    book_pages_url_with_id = fetch_fantasy_books_url_with_id()
+    book_url = 'https://tululu.org/txt.php'
+    books = []
+
+    for book_page_url, book_id in book_pages_url_with_id:
+        params = {'id': book_id}
+        try:
+            response = requests.get(book_page_url)
+            response.raise_for_status()
+            check_for_redirect(response)
+            book = parse_book_page(book_page_url, response)
+            books.append(book)
+            download_txt(book_url, params, book['title'])
+            download_image(book['image_url'])
+        except requests.exceptions.HTTPError:
+            print(f'Кажется книга №{book_id} недоступна для скачивания. '
+                  'Переходим к следующей\n', file=sys.stderr)
+            continue
+        except requests.exceptions.ConnectionError:
+            print('Возникли проблемы с сетью! Проверьте ваше соединение. '
+                  'Мы попробуем скачать следующую книгу через 10 секунд',
+                  file=sys.stderr)
+            time.sleep(10)
+            continue
+    books_json = json.dumps(books, ensure_ascii=False)
+
+    with open('books.json', 'w', encoding='utf8') as file:
+        file.write(books_json)
+
+
+if __name__ == '__main__':
+    main()
